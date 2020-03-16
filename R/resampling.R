da2eedb47f19e6ef41d50d22f14cccccf5097e5c
Evan Jiang <yifjiang@ucdavis.edu>
2020-03-16 01:21:33 -0700

    add predict.blb4m. Done with major part of the package

--- R/resampling.R
+++ R/resampling.R
@@ -1,7 +1,11 @@
 # parameters
-# r -- number of resamples
-# n -- sample size
-# subs -- subsamples returned from subsampling function
+# formula -- the regression formula
+# s -- number of subsamples
+# r -- number of bootstrap resamples
+# data -- "training" data
+# parallel -- the option to make parallization using furrr
+
+
 
 blb4lm <- function(formula, s, r, data){
   # if(is.na(data) == TRUE){
@@ -19,6 +23,7 @@ blb4lm <- function(formula, s, r, data){
   invisible(res)
 }
 
+# split the input data into s subsamples
 subsampling <- function(data, s) {
   index <- sample.int(s, nrow(data), replace = TRUE)
   data %>% split(index)
@@ -51,14 +56,18 @@ blb_sigma <- function(object, freqs){
   return(sum(e_square*freqs) / (sum(freqs) - p + 1))
 }
 
-print.blb4lm <- function(object, ...) {
+print.blb4lm <- function(object) {
   print(object)
 }
 
+# export
+# function to calculate model coefficients
 coef.blb4lm <- function(object, confidence, level){
+  # only coefficients
   coef_eachSub <- object$estimate %>% map(get_coef)
-  coef_result <- coef_eachSub %>% map(coef_res) %>% reduce(`+`) / length(coef_eachSub)
+  coef_result <- coef_eachSub %>% map(mean_coef) %>% reduce(`+`) / length(coef_eachSub)
 
+  # with confidence interval
   if(confidence == TRUE){
     quantile_eachSub <- coef_eachSub %>% map(., ~coef_quant(., level))
     coef_confint <- quantile_eachSub %>% reduce(`+`) / length(quantile_eachSub)
@@ -70,6 +79,48 @@ coef.blb4lm <- function(object, confidence, level){
   }
 }
 
+# export
+# function to calculate sigma
+sigma.blb4lm <- function(object, confidence, level){
+  sigma_eachSub <- object$estimate %>% map(get_sigma)
+  sigma_result <- sigma_eachSub %>% map(mean) %>% reduce(`+`) / length(sigma_eachSub)
+
+  if(confidence == TRUE){
+    q1 <- (1 - level)/2
+    q2 <- (1 + level)/2
+    sigma_confint <- sigma_eachSub %>% map(., ~quantile(., c(q1, q2))) %>% reduce(`+`) / length(sigma_eachSub)
+    res <- list(Sigma = sigma_result, Sigma_Confidence_Interval = sigma_confint)
+    return(res)
+  }
+  else{
+    return(sigma_result)
+  }
+}
+
+# export
+# to predict y given inputs
+predict.blb4lm <- function(object, new_data, confidence, level){
+  model_coefs <- coef.blb4lm(object, confidence = FALSE)
+  temp_predict <- apply(new_data, 1, function(d){d*model_coefs[-1]})
+  y_predict <- model_coefs[1] + apply(temp_predict, 2, sum)
+
+  if (confidence == TRUE){
+    coef_confint <- coef.blb4lm(object, confidence = TRUE, level = level)$coefficients_confidence_interval
+    int_intercept <- coef_confint[1,]
+    int_x <- apply(coef_confint[,-1], 1, function(i){
+      apply(new_data, 1, function(d){sum(d*i)})
+      })
+    int_y <- apply(int_x, 1, function(int){int + int_intercept})
+
+    res <- list(response = y_predict, response_interval = int_y)
+    return(res)
+  }
+  else{
+    return(y_predict)
+  }
+}
+
+# helper functions: invisible to users
 get_coef <- function(l){
   map(l, function(x){x$coef}) %>% reduce(cbind)
 }
@@ -80,30 +131,35 @@ coef_quant <- function(u, level){
   apply(u, 1, function(i){quantile(i, c(q1, q2))})
 }
 
-coef_res <- function(l){
+mean_coef <- function(l){
   apply(l, 1, function(i){mean(i)})
 }
 
+get_sigma <- function(l){
+  map_dbl(l, function(x){x$sigma})
+}
 
-# test
+predic_int <- function(l, intercept_int){
+  apply(l, 1, function(x){x + intercept_int})
+}
+# test code
 library(tidyverse)
 n <- 1e6
 
-test <- blb4lm(y~x, 100, 10, part1)
-t = seq_len(length(test$estimates)) %>% map(function(i) {test$estimates[[i]]})
-t1 = seq_len(length(t))
+test <- blb4lm(mpg~hp+wt, 3, 100, mtcars)
 
-func_conf <- function(l){
-  map(l, function(x){x$coef}) %>% reduce(cbind)
-}
+test_c = coef.blb4lm(test, confidence = TRUE, level = 0.95)
 
-func_sigma <- function(l){
-  map_dbl(l, function(x){x$sigma}) %>% reduce(`+`)/length(l)
-}
+sig = test$estimates %>% map(get_sigma)
 
-c = test$estimates %>% map(func_conf)
-conf <- function(u){
-  apply(u, 1, function(i){quantile(i, c(0.025, 0.975))})
+
+new_data <- as.matrix(mtcars[c(1:5),])
+new_data = new_data[,c(4, 6)]
+pred <- test_c$coefficients_confidence_interval
+# predict.blb4lm
+temp <- test_c$coefficients_confidence_interval[,-1] %>% map(., ~.*new_data) %>% reduce(cbind)
+func_predict <- function(m){
+
+
+
 }
-c %>% map(conf)
-sig = test$estimates %>% map(func_sigma)
